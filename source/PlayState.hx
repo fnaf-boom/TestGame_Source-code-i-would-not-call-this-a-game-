b688a663760add35dd78f4eefdc450ed2cb2af9f
floaty-Horizon <81320214+fnaf-boom@users.noreply.github.com>
2021-06-07 20:48:45 +0200

    Add files via upload

---
+++ source/PlayState.hx
@@ -0,0 +1,46 @@
+package;
+
+import flixel.FlxG;
+import flixel.FlxSprite;
+import flixel.FlxState;
+import flixel.text.FlxText;
+import flixel.util.FlxColor;
+
+class PlayState extends FlxState
+{
+	var text:FlxText;
+	var sprite:FlxSprite;
+
+	override public function create()
+	{
+		super.create();
+		sprite = new FlxSprite();
+		sprite.loadGraphic(AssetPaths.Lil_boi__png);
+		sprite.x = 0;
+		sprite.y = 0;
+
+		text = new FlxText(0, 0, FlxG.width, "Welcome! Enjoy!", 64);
+		text.setFormat(null, 64, FlxColor.RED, FlxTextAlign.CENTER);
+		add(text);
+	}
+
+	override public function update(elapsed:Float)
+	{
+		super.update(elapsed);
+
+		if (FlxG.keys.pressed.A || FlxG.keys.pressed.LEFT)
+			sprite.x--;
+		if (FlxG.keys.pressed.D || FlxG.keys.pressed.RIGHT)
+			sprite.x++;
+		if (FlxG.keys.pressed.W || FlxG.keys.pressed.UP)
+			sprite.y--;
+		if (FlxG.keys.pressed.S || FlxG.keys.pressed.DOWN)
+			sprite.y++;
+
+		add(sprite);
+
+		text.y++;
+		if (text.y > FlxG.height)
+			text.y = 0 - 145;
+	}
+}
